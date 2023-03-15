120dbb5da30a669ff0c5329ad44a8e1a6a9b450b
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 10:24:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in tal tills användaren anger ett negativt tal
+        // skriv sedan ut det största och minsta talet som angavs
+
+
+    }
+}
