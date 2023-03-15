d352235548ddfb9e7c60a81582296c2b9eeb26b6
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 10:38:02 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Läs in tal tills användaren anger ett negativt tal
         // skriv sedan ut det största och minsta talet som angavs
 
+        Scanner input = new Scanner(System.in);
+        int max = 0;
+        int min = Integer.MAX_VALUE;
 
+        while (true) {
+            System.out.print("Ange ett tal: ");
+            int x = input.nextInt();
+
+            if (x < 0) {
+                break;
+            }
+
+            max = Math.max(max, x);
+            min = Math.min(min, x);
+        }
+
+        System.out.println("Störst: " + max);
+        System.out.println("Minst: " + min);
     }
 }
